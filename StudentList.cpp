edb032bc4604eb4b74b7f8351317715956d97f73
SpicyRicecaker <awesomenessisgolden@gmail.com>
2019-10-07 09:10:15 -0700

    Finally figured out vector pointers git add .!

--- StudentList.cpp
+++ StudentList.cpp
@@ -1,10 +1,6 @@
 #include <iostream>
-#include <vector>
 #include <cstring>
-
-void add(vector<Student*>* stuList);
-void delete(vector<Student*>* stuList);
-void print(vector<Student*>* stuList);
+#include <vector>
 
 using namespace std;
 
@@ -15,48 +11,87 @@ struct Student{
   float gpa;
 };
 
+void add(vector<Student*>* stuList);
+void getRid(vector<Student*>* stuList);
+void print(vector<Student*>* stuList);
+void quit(bool &running);
+
 void add(vector<Student*>* stuList){
-      cout << "Please enter the first name of the student." << endl;
+  
+  cout << "Please enter the first name of the student." << endl;
 
-      stuList.push_back(Student());
+  stuList->push_back(new Student());
       
-      while(true){
-        cin.get(stuList.at(stuList.size()-1).fNm, 50);
-        cin.clear();
-        cin.ignore(999, '\n');
+  while(true){
+    cin.get(stuList->at(stuList->size()-1)->fNm, 50);
+    cin.clear();
+    cin.ignore(999, '\n');
 
-	if(stuList.at(stuList.size()-1).fNm != null){
-	  break;
-	}
+    if(stuList->at(stuList->size()-1)->fNm != NULL){
+      break;
+    }
 
-	cout << "The student's first name is empty..." << endl;
-      }
+    cout << "The student's first name is empty..." << endl;
+  }
 
-      cout << "Please enter the last name of the student" << endl;
+  cout << "Please enter the last name of the student" << endl;
       
-      while(true){
+  while(true){
 	
-        cin.get(stuList.at(stuList.size()-1).lNm, 50);
-        cin.clear();
-        cin.ignore(999, '\n');
+    cin.get(stuList->at(stuList->size()-1)->lNm, 50);
+    cin.clear();
+    cin.ignore(999, '\n');
 
-	if(stuList.at(stuList.size()-1).lNm != null){
-	  break;
-	}
+    if(stuList->at(stuList->size()-1)->lNm != NULL){
+      break;
+    }
 
-	cout << "At least put \"snow\" or something" << endl;
-      }
+    cout << "At least put \"snow\" or something" << endl;
+  }
+
+  cout << "Please enter the ID of the student" << endl;
+
+  while(true){
+
+    bool sixlen = false;
+    char in[8] = "";
+    cin.get(in, 8);
+    cin.clear();
+    cin.ignore(999, '\n');
+
+    for(int a = 0; a < strlen(in); a++){
+      
+    }
+    
+    if(stuList->at(stuList->size()-1)->lNm != NULL){
+      break;
+    }
+    int a;
+    cin >> a;
+    cin.clear();
+    cin.ignore(69420, '\n');
+    
+    cout << "Please enter a six digit number" << endl;
+  }
+  
+}
+
+void getRid(vector<Student*>* stuList){
+  
 }
 
 void print(vector<Student*>* stuList){
-  for(int a = 0; a < stuList.size(); a++){
-    cout << "First Name: " << stuList.at(a).fNm << "." << "Last Name: " << stuList.at(a).lNm << "." << "ID: " << stuList.at(a).id << "." << "GPA: " << stuList.at(a).gpa << endl;  
+  for(int a = 0; a < stuList->size(); a++){
+    cout << "First Name: " << stuList->at(a)->fNm << "." << "Last Name: " << stuList->at(a)->lNm << "." << "ID: " << stuList->at(a)->id << "." << "GPA: " << stuList->at(a)->gpa << endl;  
   }
 }
 
-void delete(vector<Student*>* stuList){
-
+void quit(bool &running){
+  cout << "Program Will Now Exit." << endl;
+  running = false;
 }
+
+
 
 int main(){
 
@@ -64,7 +99,8 @@ int main(){
   
   bool running = true;
 
-  vector <Student*>* stuList;
+  vector<Student*> stuL;
+  vector<Student*>* stuList = &stuL;
   
   while(running){
   
@@ -74,6 +110,10 @@ int main(){
       cin.get(commandIn, 8);
       cin.clear();
       cin.ignore(999, '\n');
+
+      for(int a = 0; a < strlen(commandIn); a++){
+	commandIn[a] = toupper(commandIn[a]);
+      }
       
       if(strcmp(commandIn, "ADD") == 0 || strcmp(commandIn, "PRINT") || strcmp(commandIn, "DELETE") == 0 || strcmp(commandIn, "QUIT") == 0 || strcmp(commandIn, "HELP") == 0){ 
 	break;
@@ -89,11 +129,10 @@ int main(){
       print(stuList);
     }
     else if(strcmp(commandIn, "DELETE") == 0){
-      delete(stuList);
+      getRid(stuList);
     }
     else if(strcmp(commandIn, "QUIT") == 0){
-      cout << "Program Will Now Exit." << endl;
-      running = false;
+      quit(running);
     }
     else if(strcmp(commandIn, "HELP") == 0){
       cout << "Type \"ADD\" to add a new student. Type \"DELETE\" to delete a student. Type \"QUIT\" to exit the program." << endl;
